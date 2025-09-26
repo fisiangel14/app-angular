d35c692ad1a2150511ca75dc18458f61b8becb14
Angel Alegre <fisiangel.14@gmail.com>
2025-09-26 09:33:29 -0500

    new project and first component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'example',
+    loadComponent: () => import('./pages/example/example').then(m => m.Example)
+  }
+];
---
+++ src/app/pages/example/example.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example',
+  standalone: true,
+  imports: [],
+  templateUrl: './example.html',
+  styleUrl: './example.css'
+})
+export class Example {
+
+}
